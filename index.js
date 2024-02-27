25f4e3a10f95085a6a143ac90210a17f4e1572d1
Azizul Islam <contact.azizulislam@gmail.com>
2024-02-27 23:40:12 +0600

    Running my code

--- index.js
+++ index.js
@@ -2,36 +2,51 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var compiler = require("compilex");
+require('./data') 
+
+const port = 5500;
 
 var app = express();
 app.use(bodyParser());
 
 var option = { stats: true };
 compiler.init(option);
-app.get("/", function (req, res) {
-    res.sendfile(__dirname + "/submit_code.html");
-});
+
+app.use(express.static('public'))
 
 app.post("/compilecode", function (req, res) {
     var code = req.body.code;
-    var input = req.body.input;
+    // var input = req.body.input;
     var inputRadio = req.body.inputRadio;
     var lang = req.body.lang;
     if (lang === "C" || lang === "C++") {
-        if(inputRadio == "true") {
+        let problem = problemSet[0];
+
+        if(problem['hasInput']) {
             var envData = { OS: "windows", cmd: "g++", options: {timeout:10000}};
-            compiler.compileCPPWithInput(envData, code, input, function(data) {
+            compiler.compileCPPWithInput(envData, code, problem['input'], function(data) {
                 if(data.error) {
-                    res.send(data.error);
+                    console.log(data.error);
+                    res.redirect('failed.html')
                 }
                 else {
-                    res.send(data.output);
+                    console.log("OUTPTU: "+data.output)
+                    if(data.output == problem['expectedOutput']){
+                        res.redirect('success.html')
+                    }else{
+                        res.redirect('failed.html')
+                    }
                 }
             });
             } else {
                 var envData = {OS: "windows", cmd: "g++", options: {timeout:10000}};
                 compiler.compileCPP(envData, code, function(data) {
-                    res.send(data);
+                    console.log("OUTPTU: "+data.output)
+                    if(data.output == problem['expectedOutput']){
+                        res.redirect('success.html')
+                    }else{
+                        res.redirect('failed.html')
+                    }
                 });
             }
         }
@@ -56,7 +71,7 @@ app.get("/fullStat", function(req, res) {
     });
 });
 
-app.listen(8080);
+app.listen(port);
 
 compiler.flush(function() {
     console.log("All temporary files flushed !");
